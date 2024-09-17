defb72f3e6c53f504431766f7bc4bdb225d15f9b
Pulquerio <jonas.cross@hotmail.com>
2024-09-17 13:29:30 -0300

    Eliminando o que era desnecessário no código

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World! Olá mundo!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Pastel Mania</h1>
+      <h4>O Sabor que Vícia!</h4>
+    </>
   );
 }
 
