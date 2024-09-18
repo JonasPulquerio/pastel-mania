87912f0bd497ce56f3bcb269903ec467a947b8dc
Pulquerio <jonas.cross@hotmail.com>
2024-09-18 15:17:27 -0300

    tentativa 1 de incluir carousel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+/* eslint-disable import/first */
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import {register} from 'swiper/element/bundle';
+
+register();
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 function App() {
   return (
