990e02708f072181088bc4587560ab893066e1a8
Pulquerio <jonas.cross@hotmail.com>
2024-09-18 11:41:03 -0300

    Adicionando imagens no Banner

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
@@ -7,6 +8,7 @@ function App() {
     <Header />
       <h1>Pastel Mania</h1>
       <h4>O Sabor que Vícia!</h4>
+      <Banner />
     <Footer />
     </>
   );
