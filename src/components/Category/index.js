3c793bc103c2b30a0bc8809f93ad8bec8a92563a
Pulquerio <jonas.cross@hotmail.com>
2024-10-10 00:57:20 -0300

    configuração das páginas

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -16,7 +16,7 @@ function Category({ category, children }) {
     return (
         <section className={styles.category}>
             <h1>{category}</h1>
-            <div>
+            <div className={styles.cardapio}>
                 {children}
             </div>
         </section>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,19 +2,22 @@ import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 import img from "./logotipo.png";
 
+
 function Header() {
     return (
         <header className={styles.header}>
             <div>
                 <Link to="/">
-                    <img src={img} alt="Logotipo" />
-                    <span>Pastel Mania</span>
+                    <img className={styles.logotipo} src={img} alt="Logotipo" />
+                    <span className={styles.titulo}>Pastel Mania</span>
                 </Link>
             </div>
-            <nav>
-                <Link to="/">Início</Link>
-                <Link to="/sobreNos">Sobre Nós</Link>
-                {/* <Link to="/galeria">Galeria</Link> */}
+            <nav className={styles.navMenu}>
+                <ul className={styles.menuLista}>
+                  <li><Link to="/">Início</Link></li>
+                  <li><Link to="/sobreNos">Sobre Nós</Link></li>
+                    {/* <Link to="/galeria">Galeria</Link> */}
+                </ul>
             </nav>
         </header>
     );
